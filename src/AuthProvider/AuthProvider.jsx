7093b7779224912d7e4077962b9fd29c2618db05
Chowdhurynabila <nabilachowdhuryn96@gmail.com>
2024-11-16 11:53:23 +0600

    new update, googleProvider

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 import { createContext } from "react"
 import { app } from "../firebase-config/firebase";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
 
 
@@ -9,14 +11,29 @@ export const AuthContext = createContext(null);
 const auth = getAuth(app)
 
 const AuthProvider = () => {
+    const [user]
 
-    const createUser = (email, password) =>{
+    const googleProvider = new GoogleAuthProvider();
+
+    const CreateUser = (email, password) =>{
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     const Login = (email,password) =>{
         return signInWithEmailAndPassword(auth,email, password)
-    }
+    };
+
+    const Logout = () =>{
+        return signOut(auth);
+    };
+
+    const GoogleLogin = () => {
+        return signInWithPopup(auth, googleProvider );
+    };
+
+    const authInfo = (
+        CreateUser, Login, Logout, GoogleLogin,
+    )
 
     
 
