904dab59df0123b118272d93b4e891e08b06938b
Chowdhurynabila <nabilachowdhuryn96@gmail.com>
2024-11-16 17:20:25 +0600

    AuthProvider setup updated

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,7 +1,8 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
-import { createContext } from "react"
+import { createContext, useEffect, useState } from "react"
 import { app } from "../firebase-config/firebase";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, onAuthStateChanged } from "firebase/auth";
 import { GoogleAuthProvider } from "firebase/auth/web-extension";
 
 
@@ -10,9 +11,9 @@ export const AuthContext = createContext(null);
 
 const auth = getAuth(app)
 
-const AuthProvider = () => {
-    const [user]
-
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
 
     const CreateUser = (email, password) =>{
@@ -31,15 +32,28 @@ const AuthProvider = () => {
         return signInWithPopup(auth, googleProvider );
     };
 
-    const authInfo = (
-        CreateUser, Login, Logout, GoogleLogin,
-    )
+    useEffect(() =>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+        });
+        return ( () => {
+            return unsubscribe();
+        }, []
+    );
+    });
+
+    const authInfo = {
+        user, loading, CreateUser, Login, Logout, GoogleLogin,
+
+        };
+    
 
     
 
   return (
-    <div>AuthProvider</div>
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   )
 }
 
-export default AuthProvider
+export default AuthProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import {RouterProvider,
 } from "react-router-dom";
 import { router } from './routes/routes';
+import AuthProvider from './AuthProvider/AuthProvider';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
-)
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </StrictMode>
+);
